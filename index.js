9e6d2c5ee81661d8e1516fb26c7c0e243cd7449d
azinisikhona <azizsiyalana10@gmail.com>
2024-01-17 15:11:04 +0200

    slider

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const images = [
-    ''
+    'https://i.postimg.cc/kgXwsdPR/24-SUNSET-VIEW-FROM-SUNSET-BEACH-jpg-1366x768-q85-crop-upscale.webp',
+    'https://i.postimg.cc/Z5xnT1WH/free-video-2157006.webp',
+    'https://i.postimg.cc/BbFZRyWx/BB-3bc62e96-2eb1-4c39-b4c4-3ec493a8226a-small.jpg'
   ];
 
   const intervalTime = 3000;
