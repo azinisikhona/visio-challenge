74ddac1eec39f57eb9ae91f41eb7842991cc83f9
azinisikhona <azizsiyalana10@gmail.com>
2024-02-16 12:34:27 +0200

    ...

--- index.js
+++ index.js
@@ -3,11 +3,38 @@ $(document).ready(function() {
   const intervalTime = 3000;
   let currentIndex = 0;
 
+  const pagination = $('.pagination');
+  for (let i = 0; i < carousel.children().length; i++) {
+    pagination.append('<button class="pagination-btn"></button>');
+  }
+  const paginationBtns = $('.pagination-btn');
+
+  function highlightPagination() {
+    paginationBtns.removeClass('active');
+    paginationBtns.eq(currentIndex).addClass('active');
+  }
+
+
   function changeSlide() {
     currentIndex = (currentIndex + 1) % carousel.children().length;
     const translateValue = -currentIndex * 100 + '%';
     carousel.css('transform', 'translateX(' + translateValue + ')');
+    highlightPagination();
   }
 
-  setInterval(changeSlide, intervalTime);
+
+  const interval = setInterval(changeSlide, intervalTime);
+
+
+  paginationBtns.click(function() {
+    clearInterval(interval);
+    currentIndex = $(this).index();
+    const translateValue = -currentIndex * 100 + '%';
+    carousel.css('transform', 'translateX(' + translateValue + ')');
+    highlightPagination();
+    interval = setInterval(changeSlide, intervalTime);
+  });
+
+
+  highlightPagination();
 });
